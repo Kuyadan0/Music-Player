f1bd8ae47ba82394e1a7c3b81f6866a31e1c3d71
Catalino  Adan Jr <115465428+Kuyadan0@users.noreply.github.com>
2023-01-24 20:46:58 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,57 @@
+let allMusic = [
+
+    {
+        name:"Stick with you",
+        artist: "Pussycat dolls",
+        img: "m",
+        src: "music-1"
+    },
+    {
+        name:"Best part",
+        artist: "Daniel Caesar",
+        img: "m",
+        src: "music-2"
+    },
+    {
+        name:"Be with you",
+        artist: "Akon",
+        img: "m",
+        src: "music-3"
+    },
+    {
+        name:"Double take",
+        artist: "Dhruv",
+        img: "m",
+        src: "music-4"
+    },
+    {
+        name:"Isa lang",
+        artist: "Arhtur Nery",
+        img: "m",
+        src: "music-5"
+    },
+    {
+        name:"You",
+        artist: "Basil Valdez",
+        img: "m",
+        src: "music-6"
+    },
+    {
+        name:"Im the one//slow reverb",
+        artist: "DJ Khaled",
+        img: "m",
+        src: "music-7"
+    },
+    {
+        name:"Love Melody",
+        artist: "CK YG",
+        img: "m",
+        src: "music-8"
+    },
+    {
+        name:"Your Name",
+        artist: "Sparkle",
+        img: "m",
+        src: "music-9"
+    }
+];
